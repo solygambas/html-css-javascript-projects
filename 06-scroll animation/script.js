80fd7c2224e97dafa81f867fe6979e6a1073bf5d
solygambas <solygambas@gmail.com>
2021-01-12 08:35:35 +0100

    add scroll animation

---
+++ 06-scroll animation/script.js
@@ -0,0 +1,13 @@
+const boxes = document.querySelectorAll(".box");
+
+const checkBoxes = () => {
+  const triggerBottom = (window.innerHeight / 5) * 4;
+  boxes.forEach((box) => {
+    const boxTop = box.getBoundingClientRect().top;
+    if (boxTop < triggerBottom) box.classList.add("show");
+    else box.classList.remove("show");
+  });
+};
+
+window.addEventListener("scroll", checkBoxes);
+checkBoxes();
