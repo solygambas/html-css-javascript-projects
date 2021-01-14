fe40b5ae9d0ee15e81e7d47ad086c0b516384a41
solygambas <solygambas@gmail.com>
2021-01-14 10:24:02 +0100

    add incrementing counter

---
+++ 15-incrementing counter/script.js
@@ -0,0 +1,15 @@
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach((counter) => {
+  counter.innerText = "0";
+  const updateCounter = () => {
+    const target = +counter.getAttribute("data-target");
+    const count = +counter.innerText;
+    const increment = target / 200;
+    if (count < target) {
+      counter.innerText = `${Math.ceil(count + increment)}`;
+      setTimeout(updateCounter, 1);
+    } else counter.innerText = target;
+  };
+  updateCounter();
+});
