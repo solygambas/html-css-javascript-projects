d025ca7c24aa818aeb3ee283cea32ce07181c34b
solygambas <solygambas@gmail.com>
2021-01-14 11:36:52 +0100

    add drink water

---
+++ 16-drink water/script.js
@@ -0,0 +1,44 @@
+const smallCups = document.querySelectorAll(".cup-small");
+const liters = document.getElementById("liters");
+const percentage = document.getElementById("percentage");
+const remained = document.getElementById("remained");
+
+const updateBigCup = () => {
+  const fullCups = document.querySelectorAll(".cup-small.full").length;
+  const totalCups = smallCups.length;
+  if (fullCups === 0) {
+    percentage.style.visibility = "hidden";
+    percentage.style.height = 0;
+  } else {
+    percentage.style.visibility = "visible";
+    percentage.style.height = `${(fullCups / totalCups) * 330}px`;
+    percentage.innerText = `${(fullCups / totalCups) * 100}%`;
+  }
+  if (fullCups === totalCups) {
+    remained.style.visibility = "hidden";
+    remained.style.height = 0;
+  } else {
+    percentage.style.visibility = "visible";
+    liters.innerText = `${2 - (250 * fullCups) / 1000}L`;
+  }
+};
+
+const highlightCups = (index) => {
+  if (
+    smallCups[index].classList.contains("full") &&
+    !smallCups[index].nextElementSibling.classList.contains("full")
+  ) {
+    index--;
+  }
+  smallCups.forEach((cup, index2) => {
+    if (index2 <= index) cup.classList.add("full");
+    else cup.classList.remove("full");
+  });
+  updateBigCup();
+};
+
+smallCups.forEach((cup, index) =>
+  cup.addEventListener("click", () => highlightCups(index))
+);
+
+updateBigCup();
