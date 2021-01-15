b17c6c137c900d29141ba8e168eb61a51a49e91e
solygambas <solygambas@gmail.com>
2021-01-15 08:26:09 +0100

    add movie app

---
+++ 17-movie app/script.js
@@ -0,0 +1,57 @@
+const API_URL =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1";
+// For education purpose only - Do not use this key in production
+const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCH_API =
+  'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="';
+
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+
+const getClassByRate = (vote) => {
+  if (vote >= 8) return "green";
+  else if (vote >= 7) return "orange";
+  else return "red";
+};
+
+const showMovies = (movies) => {
+  main.innerHTML = "";
+  movies.forEach((movie) => {
+    const { title, poster_path, vote_average, overview } = movie;
+    const movieElement = document.createElement("div");
+    movieElement.classList.add("movie");
+    movieElement.innerHTML = `
+    <img
+      src="${IMG_PATH + poster_path}"
+      alt="${title}"
+    />
+    <div class="movie-info">
+      <h3>${title}</h3>
+      <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+    </div>
+    <div class="overview">
+      <h3>Overview</h3>
+      ${overview}
+    </div>
+  `;
+    main.appendChild(movieElement);
+  });
+};
+
+const getMovies = async (url) => {
+  const res = await fetch(url);
+  const data = await res.json();
+  showMovies(data.results);
+};
+
+getMovies(API_URL);
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const searchTerm = search.value;
+  if (searchTerm && searchTerm !== "") {
+    getMovies(SEARCH_API + searchTerm);
+    search.value = "";
+  } else window.location.reload();
+});
