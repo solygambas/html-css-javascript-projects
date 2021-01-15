98ae88783ee2fc9e5b07c9d977a3fa9ff217fe46
solygambas <solygambas@gmail.com>
2021-01-15 13:12:11 +0100

    add background slider

---
+++ 18-background slider/script.js
@@ -0,0 +1,31 @@
+const body = document.body;
+const slides = document.querySelectorAll(".slide");
+const leftButton = document.getElementById("left");
+const rightButton = document.getElementById("right");
+
+let activeSlide = 0;
+
+const setBackground = () => {
+  body.style.backgroundImage = slides[activeSlide].style.backgroundImage;
+};
+
+const setActiveSlide = () => {
+  slides.forEach((slide) => slide.classList.remove("active"));
+  slides[activeSlide].classList.add("active");
+};
+
+rightButton.addEventListener("click", () => {
+  activeSlide++;
+  if (activeSlide > slides.length - 1) activeSlide = 0;
+  setBackground();
+  setActiveSlide();
+});
+
+leftButton.addEventListener("click", () => {
+  activeSlide--;
+  if (activeSlide < 0) activeSlide = slides.length - 1;
+  setBackground();
+  setActiveSlide();
+});
+
+setBackground();
