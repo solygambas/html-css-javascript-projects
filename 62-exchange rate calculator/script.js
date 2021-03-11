c848c9ca429253beb1678d9480c0c9f2a23a4a1b
solygambas <51904909+solygambas@users.noreply.github.com>
2021-03-11 07:42:01 +0100

    add exchange rate calculator

---
+++ 62-exchange rate calculator/script.js
@@ -0,0 +1,32 @@
+const currencyOne = document.getElementById("currency-one");
+const amountOne = document.getElementById("amount-one");
+const currencyTwo = document.getElementById("currency-two");
+const amountTwo = document.getElementById("amount-two");
+const rate = document.getElementById("rate");
+const swap = document.getElementById("swap");
+
+function calculate() {
+  const currency_one = currencyOne.value;
+  const currency_two = currencyTwo.value;
+  fetch(`https://api.exchangerate-api.com/v4/latest/${currency_one}`)
+    .then((res) => res.json())
+    .then((data) => {
+      const currentRate = data.rates[currency_two];
+      rate.innerText = `1 ${currency_one} = ${currentRate} ${currency_two}`;
+      amountTwo.value = (amountOne.value * currentRate).toFixed(2);
+    });
+}
+
+currencyOne.addEventListener("change", calculate);
+amountOne.addEventListener("input", calculate);
+currencyTwo.addEventListener("change", calculate);
+amountTwo.addEventListener("input", calculate);
+
+swap.addEventListener("click", () => {
+  const storedValue = currencyOne.value;
+  currencyOne.value = currencyTwo.value;
+  currencyTwo.value = storedValue;
+  calculate();
+});
+
+calculate();
