11b9c7324be6bcd34bd94e79c6791f08324586b0
solygambas <51904909+solygambas@users.noreply.github.com>
2021-03-12 21:38:28 +0100

    fix menu slider

--- 64-menu slider modal/script.js
+++ 64-menu slider modal/script.js
@@ -3,10 +3,26 @@ const open = document.getElementById("open");
 const close = document.getElementById("close");
 const modal = document.getElementById("modal");
 
+function closeNavbar(e) {
+  if (
+    document.body.classList.contains("show-nav") &&
+    e.target !== toggle &&
+    !toggle.contains(e.target) &&
+    e.target !== navbar &&
+    !navbar.contains(e.target)
+  ) {
+    document.body.classList.toggle("show-nav");
+    document.body.removeEventListener("click", closeNavbar);
+  } else if (!document.body.classList.contains("show-nav")) {
+    document.body.removeEventListener("click", closeNavbar);
+  }
+}
+
 // Menu Slider
-toggle.addEventListener("click", () =>
-  document.body.classList.toggle("show-nav")
-);
+toggle.addEventListener("click", () => {
+  document.body.classList.toggle("show-nav");
+  document.body.addEventListener("click", closeNavbar);
+});
 
 // Modal
 open.addEventListener("click", () => modal.classList.add("show-modal"));
