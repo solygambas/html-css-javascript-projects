13f5dcdaeaee46e9aa1bda062a0fcdb21f3e52b0
solygambas <51904909+solygambas@users.noreply.github.com>
2021-03-31 17:48:10 +0200

    add confetti options

--- 88-promo code/script.js
+++ 88-promo code/script.js
@@ -1,8 +1,19 @@
 const present = document.getElementById("present");
+const options = {
+  colors: [
+    "#34D963",
+    "#068C2C",
+    "#FF5757",
+    "#8C1414",
+    "#D9D74A",
+    "#1E91D9",
+    "#1B608C",
+  ],
+};
 
 present.addEventListener("mouseenter", () => {
-  confetti();
+  confetti(options);
 });
 present.addEventListener("touchstart", () => {
-  confetti();
+  confetti(colors);
 });
