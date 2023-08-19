1a33521962e3f0cbe7a67b81ada7848e2b9dbbd6
Blacklion567 <113174497+Blacklion567@users.noreply.github.com>
2023-08-19 19:35:27 +0800

    fix folder structure
    new method of sliding of the form and the easiest way.

--- 087-sliding signin signup form/script.js
+++ 087-sliding signin signup form/script.js
@@ -1,11 +1,21 @@
-const signUpButton = document.getElementById("signUp");
-const signInButton = document.getElementById("signIn");
+// const signUpButton = document.getElementById("signUp");
+// const signInButton = document.getElementById("signIn");
 const container = document.getElementById("container");
 
-signUpButton.addEventListener("click", () => {
+
+// This is the first method #1
+/* signUpButton.addEventListener("click", () => {
   container.classList.add("right-panel-active");
 });
 
 signInButton.addEventListener("click", () => {
   container.classList.remove("right-panel-active");
 });
+*/
+
+// This is the other method.
+container.addEventListener("click", () => {
+  container.classList.toggle("right-panel-active");
+});
+
+
