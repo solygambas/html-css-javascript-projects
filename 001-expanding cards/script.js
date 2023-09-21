cb153c95519b9b37fe6fc325819753bf9714e877
kato james <katojames248@gmail.com>
2023-09-21 16:02:00 +0300

    Added a toggle  on panel

--- 001-expanding cards/script.js
+++ 001-expanding cards/script.js
@@ -2,8 +2,8 @@ const panels = document.querySelectorAll(".panel");
 
 panels.forEach((panel) => {
   panel.addEventListener("click", () => {
-    removeActiveClasses();
-    panel.classList.add("active");
+    // removeActiveClasses();
+    panel.classList.toggle("active");
   });
 });
 
