79aafa6acd62030ff5483a05f2e71337107c03da
solygambas <51904909+solygambas@users.noreply.github.com>
2025-06-17 06:52:00 +0200

    feat: day 4

--- 004-hidden search widget/script.js
+++ 004-hidden search widget/script.js
@@ -1,8 +1,12 @@
 const search = document.querySelector(".search");
 const btn = document.querySelector(".btn");
 const input = document.querySelector(".input");
+// Animate Button Icon Change
+const icon = document.querySelector(".fas");
 
 btn.addEventListener("click", () => {
   search.classList.toggle("active");
+  icon.classList.toggle("fa-search-plus", !search.classList.contains("active"));
+  icon.classList.toggle("fa-times", search.classList.contains("active"));
   input.focus();
 });
