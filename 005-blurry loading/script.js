2e796bc5c97fe97c5aa61182fc640cbbc0ef7c81
solygambas <51904909+solygambas@users.noreply.github.com>
2025-06-17 07:26:10 +0200

    feat: day 5

--- 005-blurry loading/script.js
+++ 005-blurry loading/script.js
@@ -7,8 +7,10 @@ const blurring = () => {
   load++;
   if (load > 99) clearInterval(int);
   loadText.innerText = `${load}%`;
-  loadText.style.opacity = scale(load, 0, 100, 1, 0);
-  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+  // Fade Out Text Sooner or Later
+  loadText.style.opacity = scale(load, 0, 50, 1, 0);
+  // Modify Initial Blur Amount
+  bg.style.filter = `blur(${scale(load, 0, 100, 10, 0)}px)`;
 };
 
 // For reference: https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
@@ -16,4 +18,5 @@ const scale = (num, in_min, in_max, out_min, out_max) => {
   return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
 };
 
-let int = setInterval(blurring, 30);
+// Change the Loading Speed
+let int = setInterval(blurring, 10);
