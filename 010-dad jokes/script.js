25e4101dfacc12565b4875ea5bb0dc52dd78fdf4
solygambas <51904909+solygambas@users.noreply.github.com>
2025-06-19 12:53:43 +0200

    feat: day 10

--- 010-dad jokes/script.js
+++ 010-dad jokes/script.js
@@ -2,12 +2,20 @@ const jokeEl = document.getElementById("joke");
 const jokeBtn = document.getElementById("jokeBtn");
 
 const generateJoke = async () => {
+  // Prevent Multiple Clicks
+  jokeBtn.disabled = true;
+  jokeBtn.innerText = "Loading...";
   const config = {
     headers: { Accept: "application/json" },
   };
   const res = await fetch("https://icanhazdadjoke.com/", config);
+  // Check API Response Status
+  // const res = await fetch("https://icanhazdadjoke.com/nonexistent", config);
   const data = await res.json();
-  jokeEl.innerHTML = data.joke;
+  jokeEl.innerHTML = res.status === 200 ? data.joke : "No joke found!";
+
+  jokeBtn.disabled = false;
+  jokeBtn.innerText = "Get Another Joke";
 
   // Fetching with .then()
   //   fetch("https://icanhazdadjoke.com/", config)
