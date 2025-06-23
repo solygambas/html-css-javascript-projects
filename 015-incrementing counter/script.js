8c6a88740007f532f46fc561974058be7b4758da
solygambas <51904909+solygambas@users.noreply.github.com>
2025-06-23 20:59:03 +0200

    feat: day 15

--- 015-incrementing counter/script.js
+++ 015-incrementing counter/script.js
@@ -1,15 +1,19 @@
 const counters = document.querySelectorAll(".counter");
 
-counters.forEach((counter) => {
+counters.forEach((counter, index) => {
   counter.innerText = "0";
+
   const updateCounter = () => {
     const target = +counter.getAttribute("data-target");
     const count = +counter.innerText;
-    const increment = target / 200;
+    // Control the Animation Speed
+    const increment = target / 500;
     if (count < target) {
       counter.innerText = `${Math.ceil(count + increment)}`;
       setTimeout(updateCounter, 1);
     } else counter.innerText = target;
   };
-  updateCounter();
+
+  // Make Counters Animate Sequentially
+  setTimeout(updateCounter, index * 2500);
 });
