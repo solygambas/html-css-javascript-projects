83c852be0aca7c3266601280091c89204383d72e
solygambas <51904909+solygambas@users.noreply.github.com>
2025-06-23 13:36:55 +0200

    feat: day12

--- 012-FAQ collapse/script.js
+++ 012-FAQ collapse/script.js
@@ -2,6 +2,12 @@ const toggles = document.querySelectorAll(".faq-toggle");
 
 toggles.forEach((toggle) => {
   toggle.addEventListener("click", () => {
+    // Only One FAQ Open at a Time
+    toggles.forEach((item) => {
+      if (item !== toggle) {
+        item.parentNode.classList.remove("active");
+      }
+    });
     toggle.parentNode.classList.toggle("active");
   });
 });
