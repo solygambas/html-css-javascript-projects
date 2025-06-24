cadc870de337658f3f72e20bcf4768ba9ad2fccb
solygambas <51904909+solygambas@users.noreply.github.com>
2025-06-24 15:02:54 +0200

    feat: day 18

--- 018-background slider/script.js
+++ 018-background slider/script.js
@@ -2,8 +2,32 @@ const body = document.body;
 const slides = document.querySelectorAll(".slide");
 const leftButton = document.getElementById("left");
 const rightButton = document.getElementById("right");
+const dotsContainer = document.getElementById("dots-container");
 
 let activeSlide = 0;
+let autoplayInterval;
+let autoplayStarted = false;
+
+// Add Indicator Dots
+const createDots = () => {
+  for (let i = 0; i < slides.length; i++) {
+    const dot = document.createElement("div");
+    dot.classList.add("dot");
+    if (i === 0) dot.classList.add("active");
+
+    dot.addEventListener("click", () => {
+      activeSlide = i;
+      setBackground();
+      setActiveSlide();
+      setActiveDot();
+
+      stopAutoplay();
+      startAutoplay();
+    });
+
+    dotsContainer.appendChild(dot);
+  }
+};
 
 const setBackground = () => {
   body.style.backgroundImage = slides[activeSlide].style.backgroundImage;
@@ -14,11 +38,42 @@ const setActiveSlide = () => {
   slides[activeSlide].classList.add("active");
 };
 
+const setActiveDot = () => {
+  const dots = document.querySelectorAll(".dot");
+  dots.forEach((dot) => dot.classList.remove("active"));
+  dots[activeSlide].classList.add("active");
+};
+
+const startAutoplay = () => {
+  if (autoplayStarted) {
+    autoplayInterval = setInterval(() => {
+      rightButton.click();
+    }, 5000);
+  }
+};
+
+const stopAutoplay = () => {
+  clearInterval(autoplayInterval);
+};
+
+slides.forEach((slide) => {
+  slide.addEventListener("mouseover", stopAutoplay);
+  slide.addEventListener("mouseout", startAutoplay);
+});
+
 rightButton.addEventListener("click", () => {
   activeSlide++;
   if (activeSlide > slides.length - 1) activeSlide = 0;
   setBackground();
   setActiveSlide();
+  setActiveDot();
+
+  // Implement Autoplay
+  if (!autoplayStarted) {
+    autoplayStarted = true;
+  }
+  stopAutoplay();
+  startAutoplay();
 });
 
 leftButton.addEventListener("click", () => {
@@ -26,6 +81,11 @@ leftButton.addEventListener("click", () => {
   if (activeSlide < 0) activeSlide = slides.length - 1;
   setBackground();
   setActiveSlide();
+  setActiveDot();
+
+  stopAutoplay();
+  startAutoplay();
 });
 
+createDots();
 setBackground();
