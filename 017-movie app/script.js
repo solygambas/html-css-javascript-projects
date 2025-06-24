3077c40d33c2cc8d74bfc3b8c0a1d70fc7d2859c
solygambas <51904909+solygambas@users.noreply.github.com>
2025-06-24 11:32:47 +0200

    feat: day 17

--- 017-movie app/script.js
+++ 017-movie app/script.js
@@ -10,25 +10,37 @@ const form = document.getElementById("form");
 const search = document.getElementById("search");
 
 const getClassByRate = (vote) => {
-  if (vote >= 7.5) return "green";
-  else if (vote >= 7) return "orange";
+  // Adjust Rating Color Thresholds
+  if (vote > 8) return "green";
+  else if (vote >= 6) return "orange";
   else return "red";
 };
 
 const showMovies = (movies) => {
   main.innerHTML = "";
+  // Handle No Search Results
+  if (movies.length === 0) {
+    main.innerHTML =
+      '<h1 class="no-results">No movies found. Please try another search.</h1>';
+    return;
+  }
   movies.forEach((movie) => {
-    const { title, poster_path, vote_average, overview } = movie;
+    // Display the Movie's Release Date
+    const { title, poster_path, vote_average, overview, release_date } = movie;
+    const year = new Date(release_date).getFullYear();
     const movieElement = document.createElement("div");
     movieElement.classList.add("movie");
+    // Handle Missing Movie Posters
+    // Format Movie Ratings
     movieElement.innerHTML = `
-    <img
-      src="${IMG_PATH + poster_path}"
-      alt="${title}"
-    />
+    <img src="${
+      IMG_PATH + poster_path
+    }" alt="${title}" onerror="this.src='https://placehold.co/1280x1920/058ED9/FFF?text=${title}&font=poppins'" />
     <div class="movie-info">
-      <h3>${title}</h3>
-      <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+      <h3>${title} <span class="release-date">(${year})</span></h3>
+      <span class="${getClassByRate(vote_average)}">${vote_average.toFixed(
+      1
+    )}</span>
     </div>
     <div class="overview">
       <h3>Overview</h3>
