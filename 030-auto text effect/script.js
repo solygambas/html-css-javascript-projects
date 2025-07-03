3ecb78000500895a88ebc5443586a57b10b83acd
solygambas <51904909+solygambas@users.noreply.github.com>
2025-07-03 15:41:28 +0200

    feat: day 30

--- 030-auto text effect/script.js
+++ 030-auto text effect/script.js
@@ -1,13 +1,33 @@
 const textElement = document.getElementById("text");
 const speedElement = document.getElementById("speed");
-const text = "We Love Programming!";
+// Change the Typing Text
+// const text = "Keep calm and code on.";
+
+// Type Multiple Phrases
+const textArray = [
+  "Hello, world!",
+  "Practice makes perfect.",
+  "Keep calm and code on.",
+  "Debugging is fun.",
+  "Always write clean code.",
+  "Never stop learning.",
+];
+let phraseIndex = 0;
 let index = 1;
 let speed = 300 / speedElement.value;
 
 const writeText = () => {
-  textElement.innerText = text.slice(0, index);
+  // textElement.innerText = text.slice(0, index);
+  // index++;
+  // if (index > text.length) index = 1;
+  // setTimeout(writeText, speed);
+  const currentPhrase = textArray[phraseIndex];
+  textElement.innerText = currentPhrase.slice(0, index);
   index++;
-  if (index > text.length) index = 1;
+  if (index > currentPhrase.length) {
+    phraseIndex = (phraseIndex + 1) % textArray.length;
+    index = 1;
+  }
   setTimeout(writeText, speed);
 };
 
