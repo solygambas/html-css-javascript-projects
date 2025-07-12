86920ef8a0a55224d2d383bb97416039bb75e87d
solygambas <51904909+solygambas@users.noreply.github.com>
2025-07-12 12:32:14 +0200

    feat: day 39

--- 039-password strength background/script.js
+++ 039-password strength background/script.js
@@ -1,9 +1,19 @@
 const password = document.getElementById("password");
 const background = document.getElementById("background");
+// Optimize Performance with Throttling
+let isThrottled = false;
 
 password.addEventListener("input", (e) => {
+  if (isThrottled) return;
+  isThrottled = true;
   const input = e.target.value;
   const length = input.length;
-  const blurValue = 20 - length * 2;
-  background.style.filter = `blur(${blurValue}px)`;
+  // Adjust the Blurring Effect
+  const blurValue = 20 - length * 1;
+  // background.style.filter = `blur(${blurValue}px)`;
+  // Use a CSS Variable for the Blur Value
+  background.style.setProperty("--blur-amount", `${blurValue}px`);
+  setTimeout(() => {
+    isThrottled = false;
+  }, 100);
 });
