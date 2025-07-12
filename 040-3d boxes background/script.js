55a42f189dd5fe8cf0fe760e11c1808fd6582023
solygambas <51904909+solygambas@users.noreply.github.com>
2025-07-12 17:10:23 +0200

    feat: day 40

--- 040-3d boxes background/script.js
+++ 040-3d boxes background/script.js
@@ -1,12 +1,22 @@
 const boxesContainer = document.getElementById("boxes");
 const button = document.getElementById("btn");
+// Make the Grid Size Configurable
+// Refactor with CSS Variables
+const containerStyles = getComputedStyle(document.documentElement);
+const containerSize = parseInt(
+  containerStyles.getPropertyValue("--container-size")
+);
+const gridSize = parseInt(containerStyles.getPropertyValue("--grid-size"));
+const boxSize = containerSize / gridSize;
 
 const createBoxes = () => {
-  for (let i = 0; i < 4; i++) {
-    for (let j = 0; j < 4; j++) {
+  for (let i = 0; i < gridSize; i++) {
+    for (let j = 0; j < gridSize; j++) {
       const box = document.createElement("div");
       box.classList.add("box");
-      box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`;
+      box.style.width = `${boxSize}px`;
+      box.style.height = `${boxSize}px`;
+      box.style.backgroundPosition = `${-j * boxSize}px ${-i * boxSize}px`;
       boxesContainer.appendChild(box);
     }
   }
