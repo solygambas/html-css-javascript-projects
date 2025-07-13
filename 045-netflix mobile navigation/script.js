abf04823fd241e088d7f1b648a87470dcda73414
solygambas <51904909+solygambas@users.noreply.github.com>
2025-07-13 12:46:43 +0200

    feat: day 45

--- 045-netflix mobile navigation/script.js
+++ 045-netflix mobile navigation/script.js
@@ -1,11 +1,32 @@
 const openButton = document.querySelector(".open-btn");
 const closeButton = document.querySelector(".close-btn");
 const navs = document.querySelectorAll(".nav");
+const navMenu = navs[0];
 
-openButton.addEventListener("click", () =>
-  navs.forEach((nav) => nav.classList.add("visible"))
-);
+const closeMenu = () => {
+  navs.forEach((nav) => nav.classList.remove("visible"));
+};
 
-closeButton.addEventListener("click", () =>
-  navs.forEach((nav) => nav.classList.remove("visible"))
-);
+const openMenu = () => {
+  navs.forEach((nav) => nav.classList.add("visible"));
+};
+
+openButton.addEventListener("click", openMenu);
+
+// Close Menu on Link Click
+closeButton.addEventListener("click", closeMenu);
+
+navMenu.addEventListener("click", (e) => {
+  if (e.target.tagName === "A") closeMenu();
+});
+
+// Close Menu by Clicking Outside
+document.body.addEventListener("click", (e) => {
+  if (!e.target.closest(".nav") && !e.target.closest(".open-btn")) closeMenu();
+});
+
+// Add Keyboard Accessibility
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape" && navMenu.classList.contains("visible")) closeMenu();
+  if (e.key === "Enter" && e.target.classList.contains("open-btn")) openMenu();
+});
