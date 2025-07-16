9ed59c23211fdc0704b0623509059d33abdacf95
solygambas <51904909+solygambas@users.noreply.github.com>
2025-07-16 14:09:05 +0200

    feat: day 51

--- 051-video background/script.js
+++ 051-video background/script.js
@@ -1,7 +1,15 @@
 const menuToggle = document.querySelector(".toggle");
 const showcase = document.querySelector(".showcase");
+const video = document.querySelector(".video");
+const muteButton = document.querySelector(".mute");
 
 menuToggle.addEventListener("click", () => {
   menuToggle.classList.toggle("active");
   showcase.classList.toggle("active");
+});
+
+// Add a Mute/Unmute Button for the Video
+muteButton.addEventListener("click", () => {
+  video.muted = !video.muted;
+  muteButton.classList.toggle("muted");
 });
