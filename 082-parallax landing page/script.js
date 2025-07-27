0d433d5b927e661adeebba8cc2ec516cce32b5e6
solygambas <51904909+solygambas@users.noreply.github.com>
2025-07-27 13:35:33 +0200

    feat: day 82

--- 082-parallax landing page/script.js
+++ 082-parallax landing page/script.js
@@ -1,1 +1,19 @@
-let rellax = new Rellax(".rellax");
+const sectionGrid = document.getElementById("section-grid");
+
+// Disable Parallax on Mobile
+if (window.innerWidth >= 768) {
+  new Rellax(".rellax", {
+    callback: function (position) {
+      if (position.y < window.innerHeight / 1.4) {
+        // Create a "Reveal on Scroll" Effect
+        sectionGrid.classList.add("is-visible");
+      }
+    },
+  });
+
+  // Add a Horizontal Parallax Effect
+  new Rellax(".rellax-horizontal", {
+    horizontal: true,
+    wrapper: "#horizontal-section",
+  });
+}
