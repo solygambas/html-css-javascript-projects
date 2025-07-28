f56897f2a12dddeee592aff9d9a25c3dcb03b015
solygambas <51904909+solygambas@users.noreply.github.com>
2025-07-28 15:17:38 +0200

    feat: day 87

--- 087-sliding signin signup form/script.js
+++ 087-sliding signin signup form/script.js
@@ -1,11 +1,30 @@
 const signUpButton = document.getElementById("signUp");
 const signInButton = document.getElementById("signIn");
 const container = document.getElementById("container");
+const signUpContainer = document.getElementById("sign-up-container");
+const signInContainer = document.getElementById("sign-in-container");
+const togglePasswordIcons = document.querySelectorAll(
+  ".form-container__toggle-password i"
+);
 
-signUpButton.addEventListener("click", () => {
-  container.classList.add("right-panel-active");
-});
+function togglePanel(isSignUp) {
+  container.classList.toggle("right-panel-active", isSignUp);
+  // Improve Accessibility with ARIA
+  signUpContainer.setAttribute("aria-hidden", (!isSignUp).toString());
+  signInContainer.setAttribute("aria-hidden", isSignUp.toString());
+}
 
-signInButton.addEventListener("click", () => {
-  container.classList.remove("right-panel-active");
+// Implement Password Visibility Toggle
+function togglePasswordVisibility(icon) {
+  const input = icon.parentElement.previousElementSibling;
+  input.type = input.type === "password" ? "text" : "password";
+  icon.classList.toggle("fa-eye");
+  icon.classList.toggle("fa-eye-slash");
+}
+
+signUpButton.addEventListener("click", () => togglePanel(true));
+signInButton.addEventListener("click", () => togglePanel(false));
+
+togglePasswordIcons.forEach((icon) => {
+  icon.addEventListener("click", () => togglePasswordVisibility(icon));
 });
