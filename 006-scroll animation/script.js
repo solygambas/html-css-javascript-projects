0200c940ee284315928f8cd9c05b4427d6bca256
solygambas <51904909+solygambas@users.noreply.github.com>
2025-08-11 12:58:06 +0200

    docs: update task titles

--- 006-scroll animation/script.js
+++ 006-scroll animation/script.js
@@ -1,7 +1,7 @@
 const boxes = document.querySelectorAll(".box");
 
 const checkBoxes = () => {
-  // Adjust Scroll Trigger Point
+  // Change When Boxes Appear on Scroll
   const triggerBottom = (window.innerHeight / 5) * 3.9;
   boxes.forEach((box) => {
     const boxTop = box.getBoundingClientRect().top;
