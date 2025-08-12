034e87469d6f872bc16b435daede9a3bb706ab9a
solygambas <51904909+solygambas@users.noreply.github.com>
2025-08-12 11:44:06 +0200

    refactor: toggleActive function

--- 012-FAQ collapse/script.js
+++ 012-FAQ collapse/script.js
@@ -1,14 +1,18 @@
 const faqContainer = document.querySelector(".faq-container");
 const toggles = faqContainer.querySelectorAll(".faq-toggle");
 
+function toggleActive(selectedToggle) {
+  // Only One FAQ Open at a Time
+  toggles.forEach((toggle) => {
+    if (toggle !== selectedToggle) {
+      toggle.parentNode.classList.remove("active");
+    }
+  });
+  selectedToggle.parentNode.classList.toggle("active");
+}
+
 toggles.forEach((toggle) => {
   toggle.addEventListener("click", () => {
-    // Only One FAQ Open at a Time
-    toggles.forEach((item) => {
-      if (item !== toggle) {
-        item.parentNode.classList.remove("active");
-      }
-    });
-    toggle.parentNode.classList.toggle("active");
+    toggleActive(toggle);
   });
 });
