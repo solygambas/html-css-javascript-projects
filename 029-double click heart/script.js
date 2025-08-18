a292ca1e8594bb82fd60ebf38566033f46aac42e
solygambas <51904909+solygambas@users.noreply.github.com>
2025-08-18 18:04:25 +0200

    docs: update task titles

--- 029-double click heart/script.js
+++ 029-double click heart/script.js
@@ -1,7 +1,7 @@
 const loveMe = document.querySelector(".loveMe");
 const times = document.querySelector("#times");
 
-let clickTime = 0;
+// let clickTime = 0;
 let timesClicked = 0;
 
 const createHeart = (e) => {
