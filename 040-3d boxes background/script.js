dc35d99f88718a85a35c37a516536dec05a69f66
solygambas <51904909+solygambas@users.noreply.github.com>
2025-08-22 18:09:37 +0200

    fix: width/height logic

--- 040-3d boxes background/script.js
+++ 040-3d boxes background/script.js
@@ -1,11 +1,15 @@
 const boxesContainer = document.getElementById("boxes");
 const button = document.getElementById("btn");
-// Make the Grid Size Configurable
+
 // Refactor with CSS Variables
 const containerStyles = getComputedStyle(document.documentElement);
 const containerSize = parseInt(
   containerStyles.getPropertyValue("--container-size")
 );
+
+// Make the Grid Size Configurable
+// const gridSize = 4;
+// const boxSize = 500 / gridSize;
 const gridSize = parseInt(containerStyles.getPropertyValue("--grid-size"));
 const boxSize = containerSize / gridSize;
 
@@ -14,8 +18,9 @@ const createBoxes = () => {
     for (let j = 0; j < gridSize; j++) {
       const box = document.createElement("div");
       box.classList.add("box");
-      box.style.width = `${boxSize}px`;
-      box.style.height = `${boxSize}px`;
+      // box.style.width = `${boxSize}px`;
+      // box.style.height = `${boxSize}px`;
+      // box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`;
       box.style.backgroundPosition = `${-j * boxSize}px ${-i * boxSize}px`;
       boxesContainer.appendChild(box);
     }
